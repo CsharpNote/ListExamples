238d3261f8608277e0baa560f0199d216504a92a
Martin Kierkegaard <mark@easj.dk>
2016-10-26 19:57:44 +0200

    solution for the exercise

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -15,21 +15,21 @@ namespace Sandbox
             aListOfInt.Add(9);
 
             // Case 1
-            // Console.WriteLine("Case 1: Element with index 1 is {0}", aListOfInt[1]);
+            Console.WriteLine("Case 1: Element with index 1 is {0}", aListOfInt[1]);
 
             // Case 2
-            // Console.WriteLine("Case 2: List contains {0} elements", aListOfInt.Count);
- 
+            Console.WriteLine("Case 2: List contains {0} elements", aListOfInt.Count);
+
             aListOfInt.Add(5);
             aListOfInt.Add(22);
 
             // Case 3
-            // Console.WriteLine("Case 3: Element with index 3 is {0}", aListOfInt[3]);
+            Console.WriteLine("Case 3: Element with index 3 is {0}", aListOfInt[3]);
 
             aListOfInt.RemoveAt(0);
 
             // Case 4
-            // Console.WriteLine("Case 4: Element with index 3 is {0}", aListOfInt[3]);
+            Console.WriteLine("Case 4: Element with index 3 is {0}", aListOfInt[3]);
 
 
 
@@ -43,32 +43,55 @@ namespace Sandbox
 
             // Case 5: Make some code that prints out 
             // all the elements in the list
-
-
+            Console.WriteLine("Start of Case 5");
+            foreach (int i in aListOfInt)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("End of Case 5");
 
             // Case 6: Make some code that finds the 
             // sum of the elements in the list, and prints the result
-
+            Console.WriteLine("Start of Case 6");
+            int sum6 = 0;
+            foreach (int i in aListOfInt)
+            {
+                sum6 = sum6 + i;
+            }
+            Console.WriteLine("Sum is : {0}", sum6);
+            Console.WriteLine("End of Case 6");
 
 
             // Case 7: Make some code that finds the 
             // average of the elements in the list, and prints the result
             // Tip: Think about how average is defined; then
             // you might reuse something from Case 6
-
+            Console.WriteLine("Start of Case 7");
+            int sum7 = 0;
+            foreach (int i in aListOfInt)
+            {
+                sum7 = sum7 + i;
+            }
+            int average = sum7 / aListOfInt.Count;
+            Console.WriteLine("Average is : {0}", average);
+            Console.WriteLine("End of Case 7");
 
 
             // [DIFFICULT]
             // Case 8: Make some code that finds the
             // smallest element in the list, and prints the result
             // Tip: Think in detail about how you would do this manually
-
-
-
-
-
-
-
+            Console.WriteLine("Start of Case 8");
+            int smallest = aListOfInt[0];
+            foreach (int i in aListOfInt)
+            {
+                if (i < smallest)
+                {
+                    smallest = i;
+                }
+            }
+            Console.WriteLine("Smallest is : {0}", smallest);
+            Console.WriteLine("End of Case 8");
 
             // The LAST line of code should be ABOVE this line
         }
